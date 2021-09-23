9dc938e02eb7e9e0e06d3073ff280932ef5c25b7
ericmartinez01 <martinez.eric04@gmail.com>
2021-09-23 16:11:12 -0400

    Added const variables to grade-calc.js

--- functions/grade-calc.js
+++ functions/grade-calc.js
@@ -2,8 +2,8 @@
 // 15/20 -> You got a C (75%)
 // A 90-100, B 80-89, C 70-79, D 60-69, F 0-59
 
-let grade = function (score, totalPossibleScore) {
-  let studentsScore = (score / totalPossibleScore) * 100
+const gradeCalc = function (score, totalPossibleScore) {
+  const studentsScore = (score / totalPossibleScore) * 100
   let letterGrade = ''
   
   if (studentsScore >= 90) {
@@ -20,14 +20,5 @@ let grade = function (score, totalPossibleScore) {
   return `You got a ${letterGrade} (${studentsScore}%!)`
 }
 
-let studentOne = grade(90, 100)
-let studentTwo = grade(80, 100)
-let studentThree = grade(70, 100)
-let studentFour = grade(60, 100)
-let studentFive = grade(50, 100)
-
-console.log(studentOne)
-console.log(studentTwo)
-console.log(studentThree)
-console.log(studentFour)
-console.log(studentFive)
+const result = gradeCalc(90, 100)
+console.log(result)
