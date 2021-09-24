d94331f70c0c4c95952ce099a9d3abeba585462d
ericmartinez01 <martinez.eric04@gmail.com>
2021-09-23 20:55:33 -0400

    Created todos.js and notes.js file

---
+++ arrays/todo.js
@@ -0,0 +1,9 @@
+// Create an array with five todos
+// You have x todos
+// Print the first and second to last items -> Todo: walk the dog
+
+const todos = ['study', 'study some more', 'build a JS project', 'study some more', 'before bed time study session']
+
+console.log(`You have ${todos.length} todos`)
+console.log(`Todo: ${todos[0]}`)
+console.log(`Todo: ${todos[todos.length - 2]}`)
