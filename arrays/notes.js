5cc32ec129e4974c2bc1de83d95087106ebfc942
ericmartinez01 <martinez.eric04@gmail.com>
2021-09-23 21:40:21 -0400

    Cont... working on JS arrays
    I completed lesson 39. I was checking my work with the debugger.

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,1 +1,2 @@
 const notes = ['Note 1', 'Note 2', 'Note 3']
+
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,9 +1,12 @@
-// Create an array with five todos
-// You have x todos
-// Print the first and second to last items -> Todo: walk the dog
-
 const todos = ['study', 'study some more', 'build a JS project', 'study some more', 'before bed time study session']
 
+// Delete the 3rd item
+// Add a new item onto the end
+// Remove the first item from the list
+
+todos.splice(2, 1)
+todos.unshift('you forgot to love to study')
+todos.pop()
+
 console.log(`You have ${todos.length} todos`)
-console.log(`Todo: ${todos[0]}`)
-console.log(`Todo: ${todos[todos.length - 2]}`)
+console.log(todos)
