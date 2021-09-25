33dcca91187e0827f8e84529ef69f1b7e17320dc
ericmartinez01 <martinez.eric04@gmail.com>
2021-09-25 18:08:15 -0400

    Updated notes and todo with sort and filter arrays

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,12 +1,50 @@
-const todos = ['study', 'study some more', 'build a JS project', 'study some more', 'before bed time study session']
+const todos = [
+  {
+    text:'study',
+    completed: true
+  }, {
+    text: 'study some more',
+    completed: false
+  }, { 
+    text: 'build a JS project',
+    completed: true
+  }, {
+    text: 'buy food',
+    completed: false
+  }, {
+    text: 'before bed time study session',
+    completed: true
+  }]
 
-todos.splice(2, 1)
-todos.push('you forgot to love to study')
-todos.shift()
+const deleteTodo = function (todos, textValue) {
+    const todoIndex = todos.findIndex(function (todo) {
+    return todo.text.toLowerCase() === textValue.toLowerCase()
+  })
 
-console.log(`You have ${todos.length} todos`)
+  if (todoIndex > -1) {
+  todos.splice(todoIndex, 1)
+  }
+}
 
-todos.forEach(function (todo, index) {
-  const num = index + 1
-  console.log(`${num}. ${todo}`)
-})
+const getThingsToDo = function (todos) {
+  return todos.filter(function (todo) {
+    return !todo.completed
+  })
+}
+
+const sortTodos = function (todos) {
+  return todos.sort(function (a, b) {
+    if (!a.completed && b.completed) {
+      return -1
+    } else if (!b.completed && a.completed) {
+      return 1
+    } else {
+      return 0
+    }
+  })
+}
+
+sortTodos(todos)
+console.log(todos)
+
+// console.log(getThingsToDo(todos))
