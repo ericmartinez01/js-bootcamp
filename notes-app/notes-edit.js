3d71af587b78c53697e66ebe86dc21d740943a46
Eric <coder013579@gmail.com>
2021-10-22 14:37:43 -0400

    wired up remove-button

--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -1,31 +1,31 @@
+const titleElement = document.querySelector('#note-title') 
+const bodyElement = document.querySelector('#note-body') 
+const removeElement = document.querySelector('#remove-note')
 const noteId = location.hash.substring(1)
 const notes = getSavedNotes()
 const note = notes.find(function (note) {
   return note.id === noteId
 })
 
-if (note === undefined) { // Not working. When window refreshes it defaults to original address, therefore, this is never undefined
+if (note === undefined) { 
   location.assign('index.html')
 }
 
-document.querySelector('#note-title').value = note.title
-document.querySelector('#note-body').value = note.body
+titleElement.value = note.title
+bodyElement.value = note.body
 
-document.querySelector('#note-title').addEventListener('input', function (e) {
+titleElement.addEventListener('input', function (e) {
   note.title = e.target.value
   saveNotes(notes)
 })
 
-document.querySelector('#note-body').addEventListener('input', function (e) {
+bodyElement.addEventListener('input', function (e) {
   note.body = e.target.value
   saveNotes(notes)
 })
 
-document.querySelector('#remove-note').addEventListener('click', function (e) {
+removeElement.addEventListener('click', function (e) {
+  removeNote(note.id)
+  saveNotes(notes)
   location.assign('index.html')
 })
-
-// 1. Setup input event for title
-// 2. Update note object and save notes list
-// 3. Repeat steps 1-2 for body
-// 4. Setup a remove button that removes notes and sends user back to home page
