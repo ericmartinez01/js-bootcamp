ee1870d677a8fe2ece74619c63885b81a8787729
Eric <coder013579@gmail.com>
2021-10-22 16:35:11 -0400

    Practice with builtin Date()

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -32,3 +32,19 @@ window.addEventListener('storage', function (e) {
     renderNotes(notes, filters)
   }
 })
+
+// 1. Create two dates in the past (use string for Date)
+// 2. Get timestamps for both
+// 3. Figure out which is first and print its time (use toString)
+
+const date1 = new Date('February 14 1987')
+const date2 = new Date('November 17 1987')
+
+const timestamp1 = date1.getTime()
+const timestamp2 = date2.getTime()
+
+if (timestamp1 > timestamp2) {
+  console.log(date2.toString())
+} else {
+  console.log(date1.toString());
+}
