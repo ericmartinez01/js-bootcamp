541234df45d71498cc6256af6266401d564a1ce5
ericmartinez01 <martinez.eric04@gmail.com>
2021-10-29 14:30:44 -0400

    Create type-coercion.js

---
+++ advanced/type-coercion.js
@@ -0,0 +1,10 @@
+// Type coercion - a string, a number, a boolean
+
+console.log('5' + 5)
+console.log('5' - 5)
+console.log( '5' === 5)
+
+const value = false + 12
+const type = typeof value
+console.log(value)
+console.log(type)
