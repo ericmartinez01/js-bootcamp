297a70b95db53d64c05814fadd4667e09f9d1bba
ericmartinez01 <martinez.eric04@gmail.com>
2021-10-29 14:08:27 -0400

    Added conditional operator

--- advanced/conditional-operator.js
+++ advanced/conditional-operator.js
@@ -1,12 +1,19 @@
-const myAge = 27
-let message
+// const myAge = 27
+// const message = myAge >= 18 ? 'You can vote!' : 'You cannot vote.'
+// console.log(message)
 
+const myAge = 27 
+const showPage = () => {
+  return 'Showing the page'
+}
 
+const showErrorPage = () => {
+  return 'Showing the error page'
+}
 
-// if (myAge >= 18) {
-//   message = 'You can vote!'
-// } else {
-//   message = 'You cannote vote.'
-// }
-
+const message = myAge >= 21 ? showPage() : showErrorPage()
 console.log(message)
+
+const team = ['Tyler', 'Porter', 'Pete', 'Kerry']
+
+team.length < 4 ? console.log(`Team size: ${team.length}`) : console.log('Too many people on your team')
