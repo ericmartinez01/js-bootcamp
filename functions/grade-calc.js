ab4d08ac9d3e347f3fc34f424c7f239c96e62573
ericmartinez01 <martinez.eric04@gmail.com>
2021-10-29 15:21:52 -0400

    added try and catch

--- functions/grade-calc.js
+++ functions/grade-calc.js
@@ -2,23 +2,32 @@
 // 15/20 -> You got a C (75%)
 // A 90-100, B 80-89, C 70-79, D 60-69, F 0-59
 
-const gradeCalc = function (score, totalPossibleScore) {
-  const studentsScore = (score / totalPossibleScore) * 100
-  let letterGrade = ''
-  
-  if (studentsScore >= 90) {
-     letterGrade = 'A'
-  } else if (studentsScore >= 80) {
-    letterGrade = 'B'
-  } else if (studentsScore >= 70) {
-    letterGrade = 'C'
-  } else if (studentsScore >= 60) {
-    letterGrade = 'D'
-  } else {
-    letterGrade = 'F'
+const gradeCalc = function (score, totalScore) {
+  if (typeof score !== 'number' || typeof totalScore !== 'number') {
+    throw Error('Please provide numbers only')
   }
-  return `You got a ${letterGrade} (${studentsScore}%!)`
+    const percent = (score / totalScore) * 100
+    let letterGrade = ''
+
+    if (percent >= 90) {
+      letterGrade = 'A'
+    } else if (percent >= 80) {
+      letterGrade = 'B'
+    } else if (percent >= 70) {
+      letterGrade = 'C'
+    } else if (percent >= 60) {
+      letterGrade = 'D'
+    } else {
+      letterGrade = 'F'
+    }
+
+    return `You got a ${letterGrade} (${percent}%!)`
 }
 
-const result = gradeCalc(90, 100)
-console.log(result)
+try {
+  const result = gradeCalc('a', 100)
+  console.log(result)
+} catch (e) {
+  console.log(e.message)
+}
+
