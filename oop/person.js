484422123aad7630749a4dd472d41caa8e6a71b3
ericmartinez01 <martinez.eric04@gmail.com>
2021-11-03 16:08:39 -0400

    Created subclasses for person

--- oop/person.js
+++ oop/person.js
@@ -1,4 +1,5 @@
 // Prototypal Inheritance
+// myPerson --> Person.prototype --> Object.prototype --> null
 
 class Person {
   constructor(firstName, lastName, age, likes = []) {
@@ -22,17 +23,38 @@ class Person {
   }
 }
 
+class Employee extends Person {
+  constructor(firstName, lastName, age, position, likes) {
+    super(firstName, lastName, age, likes)
+    this.position = position
+  }
+  getBio() {
+    return `${this.firstName} ${this.lastName} is a ${position}.`
+  }
+  getYearsLeft() {
+    return 65 - this.age
+  }
+}
 
+class Student extends Person {
+  constructor(firstName, lastName, age, studentGrade){
+    super(firstName, lastName, age)
+    this.studentGrade = studentGrade
+  }
+  getBio() {
+    return (this.studentGrade >= 70) ? `${this.firstName} is passing the class` : `${this.firstName} is failing the class`
+  }
+  updateGrade(number) {
+    this.studentGrade += number
+  }
+}
 
-const me = new Person('Andrew', 'Mead', 27, ['Teaching', 'Biking'])
-me.setName = ('Alexis Turner')
-console.log(me.getBio())
+const newStudent = new Student('Andrew', 'Mead', 27, 80)
+console.log(newStudent.getBio())
+newStudent.updateGrade(-20)
+console.log(newStudent.getBio())
 
-const person2 = new Person('Clancy', 'Turner', 51)
-console.log(person2.getBio())
-
-
-// The older way to do OOP
+// ** The lesser way to do OOP **
 // const myPerson = new PersonClass('Andrew', 'Mead', 27, ['Teaching'])
 // console.log(myPerson)
 
