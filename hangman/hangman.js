39840c6183ca3a9b757a8ec964d777930fc2baf7
ericmartinez01 <martinez.eric04@gmail.com>
2021-11-03 15:33:21 -0400

    surgary syntax

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -7,32 +7,30 @@
 // Failed -> Nice try! The word was "Cat".
 // Finished -> Great work! You guessed the word.
 
-const Hangman = function (word, remainingGuesses, status) {
-  this.word = word.toLowerCase().split('')
-  this.remainingGuesses = remainingGuesses
-  this.guessedLetters = []
-  this.status = 'playing'
-}
-
-Hangman.prototype.calculateStatus = function () {
- const finished = this.word.every((letter) => this.guessedLetters.includes(letter))
-
-  if (this.remainingGuesses === 0) {
-    this.status = 'failed'
-  } else if (finished) {
-    this.status = 'finished'
-  } else {
+class Hangman {
+  constructor(word, remainingGuesses) {
+    this.word = word.toLowerCase().split('')
+    this.remainingGuesses = remainingGuesses
+    this.guessedLetters = []
     this.status = 'playing'
   }
-}
+  calculateStatus() {
+    const finished = this.word.every((letter) => this.guessedLetters.includes(letter))
 
-Hangman.prototype.getPuzzle = function () {
-  let puzzle = ''
-  this.word.forEach((letter) => this.guessedLetters.includes(letter) || letter === ' ' ? puzzle += letter : puzzle += '*')
-  return puzzle
-} 
-
-Hangman.prototype.guessTracker = function (guesses) {
+    if (this.remainingGuesses === 0) {
+      this.status = 'failed'
+    } else if (finished) {
+      this.status = 'finished'
+    } else {
+      this.status = 'playing'
+    }
+  }
+  getPuzzle() {
+    let puzzle = ''
+    this.word.forEach((letter) => this.guessedLetters.includes(letter) || letter === ' ' ? puzzle += letter : puzzle += '*')
+    return puzzle
+  }
+  guessTracker(guesses) {
     if(guesses > 1) {
       return guesses
     } else if (guesses === 1) {
@@ -40,38 +38,35 @@ Hangman.prototype.guessTracker = function (guesses) {
     } else {
       return 'You lost :('
     }
+  }
+  makeGuess(guess) {
+    guess = guess.toLowerCase()
+    const isUnique = !this.guessedLetters.includes(guess)
+    const isBadGuess = !this.word.includes(guess)
+  
+    if (this.status !== 'playing') {
+     return 
+    }
+  
+    if(isUnique) {
+      this.guessedLetters.push(guess)
+    }
+  
+    if (isUnique && isBadGuess ) {
+      this.remainingGuesses --
+    }
+    this.calculateStatus()
+  }
+  getStatus() {
+    const answer = this.word.join('')
+    if (this.status === 'failed') {
+      return `Nice try! The word was ${answer}`
+    } else if (this.status === 'playing') {
+      return `Guesses left: ${this.remainingGuesses}`
+    } else {
+      return 'Great work! You guesses the word.'
+    }
+  }
 }
-
-Hangman.prototype.makeGuess = function (guess) {
-  guess = guess.toLowerCase()
-  const isUnique = !this.guessedLetters.includes(guess)
-  const isBadGuess = !this.word.includes(guess)
-
-  if (this.status !== 'playing') {
-   return 
-  }
-
-  if(isUnique) {
-    this.guessedLetters.push(guess)
-  }
-
-  if (isUnique && isBadGuess ) {
-    this.remainingGuesses --
-  }
-  this.calculateStatus()
-}
-
-Hangman.prototype.getStatus = function () {
-  const answer = this.word.join('')
-  if (this.status === 'failed') {
-    return `Nice try! The word was ${answer}`
-  } else if (this.status === 'playing') {
-    return `Guesses left: ${this.remainingGuesses}`
-  } else {
-    return 'Great work! You guesses the word.'
-  }
-}
-
-
 
 const game1 = new Hangman('Cat', 2)
--- oop/person.js
+++ oop/person.js
@@ -1,26 +1,28 @@
 // Prototypal Inheritance
 
-const Person = function (firstName, lastName, age, likes = []) {
-  this.firstName = firstName
-  this.lastName = lastName
-  this.age = age
-  this.likes = likes
-}
-
-Person.prototype.getBio = function () {
-  let bio = `${this.firstName} is ${this.age}.`
+class Person {
+  constructor(firstName, lastName, age, likes = []) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.likes = likes
+  }
+  getBio() {
+    let bio = `${this.firstName} is ${this.age}.`
 
   this.likes.forEach((like) => {
     bio += ` ${this.firstName} likes ${like}.`
   })
   return bio
+  }
+  setName(fullName) {
+    const names = fullName.split(' ')
+    this.firstName = names[0]
+    this.lastName = names[1]
+  }
 }
 
-Person.prototype.setName = function (fullName) {
-  const names = fullName.split(' ')
-  this.firstName = names[0]
-  this.lastName = names[1]
-}
+
 
 const me = new Person('Andrew', 'Mead', 27, ['Teaching', 'Biking'])
 me.setName = ('Alexis Turner')
@@ -28,3 +30,30 @@ console.log(me.getBio())
 
 const person2 = new Person('Clancy', 'Turner', 51)
 console.log(person2.getBio())
+
+
+// The older way to do OOP
+// const myPerson = new PersonClass('Andrew', 'Mead', 27, ['Teaching'])
+// console.log(myPerson)
+
+// const Person = function (firstName, lastName, age, likes = []) {
+//   this.firstName = firstName
+//   this.lastName = lastName
+//   this.age = age
+//   this.likes = likes
+// }
+
+// Person.prototype.getBio = function () {
+//   let bio = `${this.firstName} is ${this.age}.`
+
+//   this.likes.forEach((like) => {
+//     bio += ` ${this.firstName} likes ${like}.`
+//   })
+//   return bio
+// }
+
+// Person.prototype.setName = function (fullName) {
+//   const names = fullName.split(' ')
+//   this.firstName = names[0]
+//   this.lastName = names[1]
+// }
