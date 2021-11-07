0901c514960bfc27a6d4297d68ff61b202f709fe
ericmartinez01 <martinez.eric04@gmail.com>
2021-11-07 14:42:51 -0500

    Working closures

--- hangman/app.js
+++ hangman/app.js
@@ -27,7 +27,7 @@ window.addEventListener('keypress', (e) => {
     guessEl.textContent = game1.statusMessage
 })
 
-getPuzzle((error, puzzle) => {
+getPuzzle("2", (error, puzzle) => {
   if (error) {
     console.log(`Error: ${error}`)
   } else {
@@ -35,19 +35,10 @@ getPuzzle((error, puzzle) => {
   }
 })
 
-// Making an HTTP request
-
-// const countryCode = "US"
-// const countryRequest = new XMLHttpRequest()
-
-// countryRequest.addEventListener('readystatechange', (e) => {
-//   if (e.target.readyState === 4 && e.target.status === 200) {
-//     const data = JSON.parse(e.target.responseText)
-//     const country = data.find(country => country.alpha2Code === countryCode)
-//     console.log(country.name)
-//   } else if (e.target.readyState === 4) {
-//     console.log('Unable to fetch data')
-//   }
-// })
-
-// countryRequest.open('GET', 'http://api.countrylayer.com/v2/all?access_key=71eda2a074975c79c18ba8fc2755b7b3')
+getCountry("US", (error, country) => {
+  if (error) {
+    console.log(`Error: ${error}`)
+  } else {
+    console.log(country.name)
+  }
+})
--- hangman/requests.js
+++ hangman/requests.js
@@ -1,4 +1,4 @@
-const getPuzzle = (callback) => {
+const getPuzzle = (wordCount, callback) => {
   const request = new XMLHttpRequest()
   
   request.addEventListener('readystatechange', (e) => {
@@ -10,6 +10,25 @@ const getPuzzle = (callback) => {
     }
   })
 
-  request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+  request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
   request.send()
 }
+
+// 1. Create a new function for getting country details
+const getCountry = (code, callback) => {
+  const request = new XMLHttpRequest()
+
+  request.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+      const data = JSON.parse(e.target.responseText)
+      const country = data.find(eachCountry => eachCountry.alpha2Code === code)
+      callback(undefined, country)
+    } else if (e.target.readyState === 4) {
+      callback('Unable to fetch data')
+    }
+  })
+
+  // request.open('GET', 'http://api.countrylayer.com/v2/all?access_key=71eda2a074975c79c18ba8fc2755b7b3')
+  // request.send()
+}
+
