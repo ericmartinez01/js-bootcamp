f7106ae7c2b8266a23969006f0b339601c2f6822
ericmartinez01 <martinez.eric04@gmail.com>
2021-11-12 10:51:57 -0500

    Final touches to todo-app

--- todo-app/scripts/todo-functions.js
+++ todo-app/scripts/todo-functions.js
@@ -36,6 +36,7 @@ const toggleTodo = (id) => {
 
 // Render application todos based on filters
 const renderTodos = (todos, filters) => {
+  const todoEl = document.querySelector('#todos')
   const filteredTodos = todos.filter((todo) => {
     const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
     const hideCompletedMatch = !filters.hideCompleted || !todo.completed
@@ -45,12 +46,19 @@ const renderTodos = (todos, filters) => {
 
   const incompleteTodos = filteredTodos.filter((todo) => !todo.completed)
 
-  document.querySelector('#todos').innerHTML = ''
-  document.querySelector('#todos').appendChild(generateSummaryDOM(incompleteTodos))
+  todoEl.innerHTML = ''
+  todoEl.appendChild(generateSummaryDOM(incompleteTodos))
 
-  filteredTodos.forEach((todo) => {
-    document.querySelector('#todos').appendChild(generateTodoDOM(todo))
-  })
+  if (filteredTodos.length > 0) {
+    filteredTodos.forEach((todo) => {
+      todoEl.appendChild(generateTodoDOM(todo))
+    }) 
+  } else {
+    const messageEl = document.createElement('p')
+    messageEl.classList.add('empty-message')
+    messageEl.textContent = 'No to-dos to show'
+    todoEl.appendChild(messageEl)
+  }
 }
 
 // Get the DOM elements for an individual todo
@@ -96,6 +104,8 @@ const generateTodoDOM = (todo) => {
 // Get the DOM elements for list summary
 const generateSummaryDOM = (incompleteTodos) => {
   const summary = document.createElement('h2')
-  summary.textContent = `You have ${incompleteTodos.length} todos left`
+  const plural = incompleteTodos.length === 1 ? '' : 's'
+  summary.classList.add('list-title')
+  summary.textContent = `You have ${incompleteTodos.length} todo${plural} left`
   return summary
 }
